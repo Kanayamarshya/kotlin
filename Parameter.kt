aeddfbaab71c245e3eba8dce1657ebe3e3950bab
Kanaya Marshya Saleha <150243172+Kanayamarshya@users.noreply.github.com>
2024-01-20 19:15:19 +0700

    Create Parameter.kt

---
+++ Parameter.kt
@@ -0,0 +1,22 @@
+//fungsi tanpa parameter
+fun hello(){
+    println("Kita sedang belajar Kotlin mengenal Fungsi")
+}
+
+//fungsi dengan parameter
+fun pesan(nama: String){
+    println("$nama sedang Belajar Kotlin")
+}
+
+//fungsi dengan return
+fun hitung(a : Int, b: Double, c: Int): Double{
+    return a * b + c  
+}
+
+fun main() {
+    hello()
+    pesan("Andi")
+    //println(hitung(12,10.5,2))
+    val hasil = hitung(12,10.5,2)
+    println("Hasil Total dari nilai diatas adalah $hasil")
+}
